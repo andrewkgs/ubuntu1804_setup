24afa05ff6cbf67f6f0699581de20d9cb94c8ba8
Andrew Kuo <andrewkuogs@gmail.com>
2019-04-23 20:57:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -106,6 +106,8 @@ tar -xzvf cudnn-10.0-linux-x64-v7.5.0.56.tgz
 sudo cp cuda/include/cudnn.h /usr/local/cuda/include/
 sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64/
 sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
+# sudo restart
+# watch -n 1 nvidia-smi
 
 #############################
 ## Install Python packages ##
