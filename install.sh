63a9cc66306e7f5c050653c6fbfe6f984f2a217f
Andrew Kuo <andrewkuogs@gmail.com>
2019-04-24 19:14:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -96,7 +96,7 @@ sudo apt install nvidia-384
 
 # https://developer.nvidia.com/cuda-downloads
 wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64
-sudo dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64
 sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
 sudo apt-get update
 sudo apt-get install cuda
