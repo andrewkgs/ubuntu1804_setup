fec97026409a6db5297b23169fb4d1feed7fa7c6
Andrew Kuo <andrewkuogs@gmail.com>
2020-03-02 16:50:13 +0800

    Create conda_command.sh

---
+++ conda_command.sh
@@ -0,0 +1,4 @@
+conda create --name inpaint python=3.7 numpy=1.16.4
+conda install -c pytorch pytorch torchvision
+conda install -c conda-forge tensorflow-gpu tensorboardx opencv ipdb
+conda install -c anaconda pyyaml
